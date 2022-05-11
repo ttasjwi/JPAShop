073ef026ea758bb59fa590d9797f3b7f89eabf7d
ttasjwi <ttasjwi920@gmail.com>
2022-05-11 15:21:17 +0900

    정적 리소스, 홈 컨트롤러 추가

---
+++ src/main/java/jpa/book/JPAShop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package jpa.book.JPAShop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
