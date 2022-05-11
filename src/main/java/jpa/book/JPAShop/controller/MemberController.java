af81e09b22a8d23d3490bd262848b86b75648ba8
ttasjwi <ttasjwi920@gmail.com>
2022-05-11 15:57:03 +0900

    회원 가입 기능 구현
    - createForm : form 페이지 반환
    - create : 회원 가입

---
+++ src/main/java/jpa/book/JPAShop/controller/MemberController.java
@@ -0,0 +1,43 @@
+package jpa.book.JPAShop.controller;
+
+import jpa.book.JPAShop.domain.Address;
+import jpa.book.JPAShop.domain.Member;
+import jpa.book.JPAShop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/form")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping
+    public String create(@Validated MemberForm memberForm, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
